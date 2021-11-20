c688f4377dbca17330420a4e470362881caa94c1
Aditya Vijayraj <62451961+AdityaVijayraj@users.noreply.github.com>
2021-11-20 18:26:02 +0530

    Update home.js

--- home.js
+++ home.js
@@ -3,15 +3,6 @@ $(window).on('load', function () {
     $(".loader-wrapper").fadeOut("fast");
 });
 
-// to start animation after page load
-document.body.classList.add('js-loading');
-
-window.addEventListener("load", showPage);
-
-function showPage() {
-  document.body.classList.remove('js-loading');
-}
-
 // to keep viewport height constant with virtual keyboard on phones
 var isAndroid = navigator.userAgent.toLowerCase().indexOf("android") > -1;
         if (isAndroid) {
